bff329d352cf3a05bc33867992da715a1476a42f
adomigold <adolphgasper@gmail.com>
2022-07-29 00:46:05 +0300

    remove line on links on cast profile page

--- src/components/shared/PersonTvAndMovies.jsx
+++ src/components/shared/PersonTvAndMovies.jsx
@@ -8,7 +8,7 @@ export default function PersonTvAndMovies({route, media_credits, divID, style })
             {media_credits.map(c => (
                 <div key={c.id} className="col-md-2 col-4">
                     <Link href={`/${route}${c.id}-${route === 'watch/tv?q=' ? '1-' : ''}${c.original_title ? c.original_title : c.original_name}`}>
-                        <a>
+                        <a style={{"textDecoration": "none"}}>
                             <figure classNam="figure">
                                 <Image className="rounded-1 figure-img" width="130px" height="195px" alt={c.title}
                                     src={c.poster_path ? `/api/getImage?q=${c.poster_path}` : '/img/errors/grey.jpg'} />
