c9fdaf16bc9c747c8643c1831cc035b1148a840b
adomigold <adolphgasper@gmail.com>
2022-07-29 00:52:40 +0300

    fix typo

--- src/components/shared/PersonTvAndMovies.jsx
+++ src/components/shared/PersonTvAndMovies.jsx
@@ -9,7 +9,7 @@ export default function PersonTvAndMovies({route, media_credits, divID, style })
                 <div key={c.id} className="col-md-2 col-4">
                     <Link href={`/${route}${c.id}-${route === 'watch/tv?q=' ? '1-' : ''}${c.original_title ? c.original_title : c.original_name}`}>
                         <a style={{"textDecoration": "none"}}>
-                            <figure classNam="figure">
+                            <figure className="figure">
                                 <Image className="rounded-1 figure-img" width="130px" height="195px" alt={c.title}
                                     src={c.poster_path ? `/api/getImage?q=${c.poster_path}` : '/img/errors/grey.jpg'} />
                                 <figcaption className="figure-caption">{c.title ? c.title : c.name}</figcaption>
