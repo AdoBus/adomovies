5b6890228fcd3d7ce9a9e1dcc2045fc3b773b0a9
adomigold <adolphgasper@gmail.com>
2022-08-24 15:58:01 +0300

    add carousel on casts component

--- src/components/shared/CastsComponent.jsx
+++ src/components/shared/CastsComponent.jsx
@@ -1,31 +1,57 @@
 import Image from "next/image"
 import Link from "next/link"
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 6
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 6
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 4
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 2
+    }
+};
 
 export default function Casts({ movie }) {
     return (
         <>
-            <div className="row g-4">
-                <div className="d-flex align-items-end align-items-lg-center justify-content-between pb-md-2">
-                    <div className="d-flex w-100 align-items-center justify-content-between justify-content-lg-start">
-                        <h2 className="h5 mb-0 me-md-4 text-light">Main Casts</h2>
-                    </div>
+            <div className="d-flex align-items-end align-items-lg-center justify-content-between pb-md-2">
+                <div className="d-flex w-100 align-items-center justify-content-between justify-content-lg-start">
+                    <h2 className="h5 mb-0 me-md-4 text-light">Main Casts</h2>
                 </div>
-                {movie.credits.cast.slice(0, 6).map(c => (
-                    <div key={c.name} className="col-md-2 col-6">
+            </div>
+            <Carousel
+                responsive={responsive}
+                swipeable={true}
+                ssr={true}
+                infinite={false}
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+            >
+                {movie.credits.cast.map(c => (
+                    <div key={c.name} className="mt-3 me-3">
                         <Link href={`/person?q=${c.id}-${c.name}`}>
-                            <a className="card card-light h-100 shadow-sm border-0">
-                                <div className="card-img-top card-img-hover">
-                                    <Image width="178" height="205.5" src={c.profile_path ? `/api/getImage?q=${c.profile_path}` : '/img/errors/grey.jpg'} alt={c.name} />
-                                </div>
-                                <div className="card-body text-center">
-                                    <p className="mb-0 fs-sm"><strong>{c.name}</strong></p>
-                                    <p className="mb-0 fs-sm text-light opacity-70">{c.character}</p>
-                                </div>
+                            <a>
+                                <figure className="figure">
+                                    <Image className="rounded-1 figure-img" width="178" height="287.5"
+                                        src={c.profile_path ? `/api/getImage?q=${c.profile_path}` : '/img/errors/grey.jpg'} alt={c.name} />
+                                    <figcaption className="figure-caption text-light"><strong>{c.name}</strong></figcaption>
+                                    <figcaption className="figure-caption">{c.character}</figcaption>
+                                </figure>
                             </a>
                         </Link>
                     </div>
                 ))}
-            </div>
+            </Carousel>
         </>
     )
 }
--- src/components/shared/TopReview.jsx
+++ src/components/shared/TopReview.jsx
@@ -5,7 +5,7 @@ import moment from "moment";
 export default function TopReview({ movie }) {
     return (
         <>
-            <div className="mt-5 d-flex align-items-end align-items-lg-center justify-content-between pb-md-2">
+            <div className="mt-3 d-flex align-items-end align-items-lg-center justify-content-between pb-md-2">
                 <div className="d-flex w-100 align-items-center justify-content-between justify-content-lg-start">
                     <h2 className="h5 mb-0 me-md-4 text-light">Top Review</h2><span className="badge bg-info fs-xs">TMDB</span>
                 </div>
